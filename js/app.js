17bd194e9a04a90654ec1c6b7f0b14ccbfb47b4f
heyjinkim <kim.heyjin@gmail.com>
2013-07-08 00:33:51 -0400

    added data.js

--- js/app.js
+++ js/app.js
@@ -1,33 +1,4 @@
-var data = [
-    {  "letter":"A", "frequency":0.08167 },
-    {  "letter":"B", "frequency":0.01492 },
-    {  "letter":"C", "frequency":0.02780 },
-    {  "letter":"D", "frequency":0.04253 },
-    {  "letter":"E", "frequency":0.12702 },
-    {  "letter":"F", "frequency":0.02288 },
-    {  "letter":"G", "frequency":0.02022 },
-    {  "letter":"H", "frequency":0.06094 },
-    {  "letter":"I", "frequency":0.06973 },
-    {  "letter":"J", "frequency":0.00153 },
-    {  "letter":"K", "frequency":0.00747 },
-    {  "letter":"L", "frequency":0.04025 },
-    {  "letter":"M", "frequency":0.02517 },
-    {  "letter":"N", "frequency":0.06749 },
-    {  "letter":"O", "frequency":0.07507 },
-    {  "letter":"P", "frequency":0.01929 },
-    {  "letter":"Q", "frequency":0.00098 },
-    {  "letter":"R", "frequency":0.05987 },
-    {  "letter":"S", "frequency":0.06333 },
-    {  "letter":"T", "frequency":0.09056 },
-    {  "letter":"U", "frequency":0.02758 },
-    {  "letter":"V", "frequency":0.01037 },
-    {  "letter":"W", "frequency":0.02465 },
-    {  "letter":"X", "frequency":0.00150 },
-    {  "letter":"Y", "frequency":0.01971 },
-    {  "letter":"Z", "frequency":0.00074 }
-  ];
-
-/* D3 value definitions */
+/* D3 value definition */
 var margin = {top: 20, right: 20, bottom: 30, left: 40},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
@@ -38,11 +9,11 @@ var y = d3.scale.linear().range([height, 0]);
 var xAxis = d3.svg.axis().scale(x).orient("bottom");
 var yAxis = d3.svg.axis().scale(y).orient("left").tickFormat(formatPercent);
 
-x.domain(data.map(function(d) { return d.letter; }));
-y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
+x.domain(DATA.map(function(d) { return d.letter; }));
+y.domain([0, d3.max(DATA, function(d) { return d.frequency; })]);
 
 
-/* Ember code using Component */
+/* Ember.Component */
 App = Ember.Application.create();
 
 App.BarChartComponent = Ember.Component.extend({
@@ -52,7 +23,7 @@ App.BarChartComponent = Ember.Component.extend({
   transformX: "translate(0,"+height+")",
 
   model: function(){ 
-    var datum = data.map(function(d, i){
+    var data = DATA.map(function(d, i){
       return {
           x: x(d.letter), 
           y: y(d.frequency), 
@@ -61,7 +32,7 @@ App.BarChartComponent = Ember.Component.extend({
          };
     });
     
-    return datum;
+    return data;
   }.property(),
 
   didInsertElement: function(){
